63dd2050575138267afb92243a0cac5692707334
上帝的梦 <1597691532@qq.com>
2025-08-19 17:42:20 +0800

    1

--- application.bb471.js
+++ application.bb471.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.12094.json';
+          this.settingsPath = 'src/settings.02a1e.json';
           this.showFPS = false;
         }
 
--- index.c57d9.js
+++ index.c57d9.js
@@ -1,4 +1,4 @@
-System.register(["./application.62cc3.js"], function (_export, _context) {
+System.register(["./application.bb471.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.62cc3.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application62cc3Js) {
-      Application = _application62cc3Js.Application;
+    setters: [function (_applicationBb471Js) {
+      Application = _applicationBb471Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
