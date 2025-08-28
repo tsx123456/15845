66abc40c7c78f1d1916e0406fbb66568a3b95033
上帝的梦 <1597691532@qq.com>
2025-08-28 16:45:04 +0800

    1

--- application.47364.js
+++ application.47364.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.69485.json';
+          this.settingsPath = 'src/settings.256e0.json';
           this.showFPS = false;
         }
 
--- index.c2fb8.js
+++ index.c2fb8.js
@@ -1,4 +1,4 @@
-System.register(["./application.3482f.js"], function (_export, _context) {
+System.register(["./application.47364.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.3482f.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application3482fJs) {
-      Application = _application3482fJs.Application;
+    setters: [function (_application47364Js) {
+      Application = _application47364Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
